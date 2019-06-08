4d9b039cce2f9f9a97e57d3f9ca3c2b56df79796
yaroslav-bodnar <50916274+yaroslav-bodnar@users.noreply.github.com>
2019-06-08 15:12:26 +0300

    Part three!
    Connected popup form via javascript!

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+var header = document.getElementsByClassName("header");
+var main = document.getElementsByClassName("main");
+// var main = document.getElementsByTagName("main");---ищет и подключает теги!!!
+// var main = document.getElementsById("main"); --- ищет и подключает классы!!!
+var hat = document.getElementsByClassName("hat");
+// или
+// var hat = document.querySelector(".hat");
+
+console.log(header);
+console.log(main);
+console.log(hat);
+
+
+
+
+
+// Вызов модального окна - формы для пользователя
+$(document).ready(function(){
+
+	$('.header__contacts__buttons').on("click", function(){
+		$('.dimming-popup-form').show()
+	});
+	
+	$('.dimming-popup-form__popup-form__close').on("click", function(){
+		$('.dimming-popup-form').hide()
+	});
+
+});
